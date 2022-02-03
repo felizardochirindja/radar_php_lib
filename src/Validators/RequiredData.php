ed3301c066f588fe9e3389525136b2bbb82fc13a
felizardochirindja <felizardo.chirindja@gmail.com>
2022-02-03 21:03:46 +0200

    comentando e anotando funções da classe RequiredData

--- src/Validators/RequiredData.php
+++ src/Validators/RequiredData.php
@@ -19,8 +19,12 @@ final class RequiredData extends Validator implements Validatable
     }
 
     /**
+     * valida um nome
+     * 
      * @param string $name nome a ser validado
      * @param string $error erro caso o nome seja inválido
+     * 
+     * @return array
     */
     public function validateName(string $name, string $error) : array
     {
@@ -47,8 +51,12 @@ final class RequiredData extends Validator implements Validatable
     }
     
     /**
+     * valida um número
+     * 
      * @param string $number número a ser validado
      * @param string $error erro caso o número seja inválido
+     * 
+     * @return array
     */
     public function validateNumber(int | string $number, string $error) : array
     {
@@ -75,8 +83,12 @@ final class RequiredData extends Validator implements Validatable
     }
 
     /**
+     * valida um email
+     * 
      * @param string $email email a ser validado
      * @param string $error erro caso o email seja inválido
+     * 
+     * @param array
     */
     public function validateEmail(string $email, string $error) : array
     {
@@ -103,8 +115,12 @@ final class RequiredData extends Validator implements Validatable
     }
 
     /**
+     * valida uma URL
+     * 
      * @param string $url url a ser validada
      * @param string $error erro caso a url seja inválida
+     * 
+     * @return array
     */
     public function validateURL(string $url, string $error) : array
     {
@@ -131,8 +147,13 @@ final class RequiredData extends Validator implements Validatable
     }
 
     /**
+     * valida um ou uma cadeia de caracteres de modo que contenha
+     * o numero de caracteres desejados, invalidando tags HTML e espaços em branco
+     * 
      * @param string $chars caracteres a serem validados
      * @param string $error erro caso os caracteres sejam inválidos
+     * 
+     * @return array
     */
     public function validateString(int | string $chars) : array
     {
@@ -156,6 +177,15 @@ final class RequiredData extends Validator implements Validatable
         return $data;
     }
 
+    /**
+     * returna um array com string vazia em todos os indices,
+     * caso seja entrege um dado vazio
+     * 
+     * @param int|string $givenData data to be verified if is empty, caso esteja vazio é retornado um array a mensagem de erro no índice "error"
+     * @param string $dataType type of data to be validated, that will be used as a index of first item in the array (types normaly used: name, url, email, password)
+     * 
+     * @return array|false
+    */
     protected function validateEmptyData(
         int | string $givenData, 
         string $dataType) : array | false
@@ -174,6 +204,11 @@ final class RequiredData extends Validator implements Validatable
         return false;
     }
 
+    /**
+     * se o erro génerico estiver vazio, é usado o erro definido pela função setError
+     * caso o erro genério não esteja vazio será usado ele mesmo como erro quando
+     * se o um dado estiver vazio
+    */
     private function setRequiredDataError() : void
     {
         $this->requiredDataError = empty($this->requiredDataError) ? 
@@ -181,12 +216,17 @@ final class RequiredData extends Validator implements Validatable
                                    $this->requiredDataError;
     }
 
+    /**
+     * limpa a mensagem de erro depois de cada validação
+    */
     private function removeRequiredDataError(): void
     {
         $this->requiredDataError = "";
     }
 
     /**
+     * insere a mensagem de erro retornada caso o dado o dado seja vazios
+     * 
      * @param string $error erro caso o seja um dado vazio
     */
     public function setError(string $error) : void
