d51ac33457c4d1a393242b97c541e0242baebb6e
felizardochirindja <felizardo.chirindja@gmail.com>
2022-02-03 18:39:52 +0200

    isNumberNegative function arranged

--- tests/Core/ValidatorTest.php
+++ tests/Core/ValidatorTest.php
@@ -8,7 +8,11 @@ require "../../vendor/autoload.php";
 class ValidatorTest extends TestCase
 {
     public function isNumberNegativeTest() {
-
+        // arrange
+        $validatorMock = $this->getMockForAbstractClass(Validator::class);
+        $isNumberNegativeMethod = self::getPrivateMethod($validatorMock, 'isNumberNegative');
+        
+        
     }
 
     public function sanitizeDataTest() {
