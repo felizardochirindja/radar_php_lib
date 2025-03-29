41fd1f5c01d026c415f3bedc52b287216f0d47ed
felizardochirindja <felizardo.chirindja@gmail.com>
2025-03-30 00:19:04 +0200

    feat: enhance DataFilter enum with filter retrieval method and detailed comments

--- src/Core/DataFilter.php
+++ src/Core/DataFilter.php
@@ -2,8 +2,28 @@
 
 namespace Radar\Core;
 
+/**
+ * Define os tipos de filtros suportados
+ */
 enum DataFilter: int
 {
-    case Email = FILTER_VALIDATE_EMAIL;
-    case Url = FILTER_VALIDATE_URL;
+    /** 
+     * @var int Filtro para validação de e-mail 
+     * O valor numérico associado refere-se à constante FILTER_VALIDATE_EMAIL nativa PHP.
+     */
+    case Email = 274;
+
+    /** 
+     * @var int Filtro para validação de URL 
+     * O valor numérico associado refere-se à constante FILTER_VALIDATE_URL nativa PHP.
+     */
+    case Url = 273;
+
+    public function getFilter(): int
+    {
+        return match ($this) {
+            self::Email => FILTER_VALIDATE_EMAIL,
+            self::Url => FILTER_VALIDATE_URL,
+        };
+    }
 }
